df13971f27c93bb6972a95825ea29b3ddc90af60
trungluong <trungluong>
2023-04-10 16:13:18 +0700

    fix

--- constants/upload.ts
+++ constants/upload.ts
@@ -3,6 +3,7 @@ export default {
   sizeFile: {
     photo: 3000000,
     video: 10000000,
+    file: 10000000,
   },
   extension: {
     photo: ["jpeg", "jpg", "png", "JPG", "PNG"],
@@ -14,12 +15,21 @@ export default {
       height: 300,
     },
     photo: {
-      width: 720,
-      height: 1280,
+      width: 1280,
+      height: 720,
+    },
+    video: {
+      width: 1280,
+      height: 720,
     },
   },
   contentTypePrefix: {
     image: "image/",
     video: "video/",
+  },
+  type: {
+    photo: "photo",
+    video: "video",
+    file: "file",
   },
 };
---
+++ ultils/pfile/video.ts
@@ -0,0 +1,7 @@
+import ffmpeg from "fluent-ffmpeg";
+
+const resize = async (inputPath: string, outputPath: string, width: number, height: number) => {
+  return new Promise((resolve, reject) => {
+
+  });
+};
